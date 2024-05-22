f1ef5ee336eeafa95fc1536d3001ec64cfc8b2af
tuha <tuha263@gmail.com>
2024-05-22 17:29:23 +0700

    Update gacha package

---
+++ UnityFeatureModule/Assets/GameModules/Gacha/Editor/GameFeatureSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Configs.GameEvents
+{
+    using Sirenix.OdinInspector;
+
+    public partial class GameFeaturesSetting
+    {
+
+        [FoldoutGroup("Gacha")] [BoxGroup("Gacha")]
+        public bool TestSetting;
+    }
+}
