d5fb573e022b842c64c797e2c4ab0c1e2108a938
Nguyễn Mạnh <75465287+frostbun@users.noreply.github.com>
2024-06-13 15:27:56 +0700

    change playfab leaderboard flow (#4)

--- UnityFeatureModule/Packages/HighScore/Scripts/AutoSyncService.cs
+++ UnityFeatureModule/Packages/HighScore/Scripts/AutoSyncService.cs
@@ -1,7 +1,6 @@
 #nullable enable
 namespace TheOneStudio.HighScore
 {
-    using Cysharp.Threading.Tasks;
     using TheOneStudio.HighScore.Models;
     using UniT.Extensions;
     using UnityEngine.Scripting;
@@ -23,35 +22,28 @@ namespace TheOneStudio.HighScore
 
         public void Initialize()
         {
-            if (this.leaderboardManager.IsInitialized)
-            {
-                InitializeInternal();
-            }
-            else
-            {
-                this.leaderboardManager.OnInitialized += InitializeInternal;
-            }
+            this.leaderboardManager.OnInitialized += InitializeInternal;
 
             void InitializeInternal()
             {
-                this.highScoreManager.OnNewHighScore += this.OnNewHighScore;
                 this.SyncHighScore();
+                this.highScoreManager.OnNewHighScore  += this.OnNewHighScore;
+                this.leaderboardManager.OnInitialized -= InitializeInternal;
             }
+        }
+
+        private void SyncHighScore()
+        {
+            this.leaderboardConfig.UsedValues.ForEach((key, type) =>
+            {
+                var entry = this.leaderboardManager.GetCachedCurrentPlayerEntry(key, type)!;
+                this.highScoreManager.Submit(key, type, entry.Score);
+            });
         }
 
         private void OnNewHighScore(string key, HighScoreType type, int oldHighScore, int newHighScore)
         {
             this.leaderboardManager.SubmitAsync(key, type, newHighScore);
         }
-
-        private void SyncHighScore() => UniTask.Void(async () =>
-        {
-            await this.leaderboardManager.FetchAllUsedValuesAsync();
-            this.leaderboardConfig.UsedValues.ForEach((key, type) =>
-            {
-                var entry = this.leaderboardManager.GetCachedCurrentPlayerEntry(key, type)!;
-                this.highScoreManager.Submit(key, type, entry.Score);
-            });
-        });
     }
 }
